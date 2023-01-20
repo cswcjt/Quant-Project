a01e6f53cbacddf7000eb2d86ca47bca53349f1b
cswcjt <jungtaek0227@gmail.com>
2023-01-20 16:57:32 +0900

    refact: Adjust for Rebalancing

--- quant/strategy/optimize/CrossSectional.py
+++ quant/strategy/optimize/CrossSectional.py
@@ -1,115 +1,314 @@
+# 패키지 임포트
 import numpy as np
 import pandas as pd
-from scipy.stats import norm
+import yfinance as yf
+from pandas.tseries.offsets import *
+
 from scipy.optimize import minimize
+from scipy.stats import norm
 
-# 횡적 배분 모델 클래스 
-class CrossSectional:
-    def __init__(self, vol, er, cov):
-        self.vol = vol
-        self.er = er
-        self.cov = cov
 
-    # EW
+class Equalizer:
+    # 횡적 배분 모델 클래스
+    def __init__(self, signal: pd.DataFrame, rebal_price: pd.DataFrame, param: int) -> pd.DataFrame:
+
+        # 팩터의 시그널
+        self.signal = signal
+
+        # 연율화 패러미터
+        self.param = param
+
+        # 총 투자자산의 개수
+        self.noa = len(rebal_price.columns)
+
+        # 리밸런싱별 수익률
+        self.rets = self.signal * rebal_price.pct_change().dropna()
+
+        # 기대수익률(연율화 수익률): 12개월
+        self.er = np.array(self.rets.mean() * self.param)
+
+        # 연율화 변동성: 12개월 
+        self.vol = np.array(self.rets.std() * np.sqrt(self.param))
+
+        # 연율화 공분산행렬: 12개월
+        self.cov = self.rets.cov().dropna() * self.param
+
+    # BETA(buy and hold 가중 함수)
+    def beta(self):
+        weights = self.signal.copy()
+        weights.iloc[:] = 1 / self.noa
+
+        return weights
+    
+    # EW(동일 비중 가중치 계산 함수)
     def ew(self):
-        noa = self.er.shape[0]
-        weights = np.ones_like(self.er) * (1/noa)
+        weights = self.signal.apply(lambda series: series / series.sum(), axis=1)
+
         return weights
-    
-    # MSR
-    def msr(self):
-        noa = self.er.shape[0]
-        init_guess = np.repeat(1/noa, noa)
 
-        bounds = ((0.0, 1.0), ) * noa
-        weights_sum_to_1 = {'type': 'eq',
-                            'fun': lambda weights: np.sum(weights) - 1}
-
-        def neg_sharpe(weights, er, cov):
-            r = weights.T @ er
-            vol = np.sqrt(weights.T @ cov @ weights)
-            return - r / vol
-
-        weights = minimize(neg_sharpe,
-                           init_guess,
-                           args=(self.er, self.cov),
-                           method='SLSQP',
-                           constraints=(weights_sum_to_1,), 
-                           bounds=bounds)
-
-        return weights.x
-    
-    # GMV
-    def gmv(self):
-        noa = self.cov.shape[0]
-        init_guess = np.repeat(1/noa, noa)
-
-        bounds = ((0.0, 1.0), ) * noa
-        weights_sum_to_1 = {'type': 'eq',
-                            'fun': lambda weights: np.sum(weights) - 1}
-
-        def port_vol(weights, cov):
-            vol = np.sqrt(weights.T @ cov @ weights)
-            return vol
-
-        weights = minimize(port_vol, init_guess, args=(self.cov), method='SLSQP', constraints=(weights_sum_to_1,), bounds=bounds)
-
-        return weights.x
-    
-    # MDP
-    def mdp(self):
-        noa = self.vol.shape[0]
-        init_guess = np.repeat(1/noa, noa)
-        bounds = ((0.0, 1.0), ) * noa
-        
-        weights_sum_to_1 = {'type': 'eq',
-                            'fun': lambda weights: np.sum(weights) - 1}
-        
-        def neg_div_ratio(weights, vol, cov):
-            weighted_vol = weights.T @ vol
-            port_vol = np.sqrt(weights.T @ cov @ weights)
-            return - weighted_vol / port_vol
-        
-        weights = minimize(neg_div_ratio,
-                           init_guess, 
-                           args=(self.vol, self.cov),
-                           method='SLSQP',
-                           constraints=(weights_sum_to_1,),
-                           bounds=bounds)
-        
-        return weights.x
-    
-    # RP
-    def rp(self):
-        noa = self.cov.shape[0]
-        init_guess = np.repeat(1/noa, noa)
-        bounds = ((0.0, 1.0), ) * noa
-        target_risk = np.repeat(1/noa, noa)
-        
-        weights_sum_to_1 = {'type': 'eq',
-                    'fun': lambda weights: np.sum(weights) - 1}
-        
-        def msd_risk(weights, target_risk, cov):
-            
-            port_var = weights.T @ cov @ weights
-            marginal_contribs = cov @ weights
-            
-            risk_contribs = np.multiply(marginal_contribs, weights.T) / port_var
-            
-            w_contribs = risk_contribs
-            return ((w_contribs - target_risk)**2).sum()
-        
-        weights = minimize(msd_risk,
-                           init_guess,
-                           args=(target_risk, self.cov),
-                           method='SLSQP',
-                           constraints=(weights_sum_to_1,),
-                           bounds=bounds)
-        return weights.x
-    
-    # EMV
+    # EMV(역변동성)
     def emv(self):
-        inv_vol = 1 / self.vol
-        weights = inv_vol / inv_vol.sum()
+        temp_weights = self.vol * self.signal
+        weights = temp_weights.apply(lambda series: series / series.sum(), axis=1)
 
         return weights
+
+
+class Optimization(Equalizer):
+    def __init__(self, signal: pd.DataFrame, rebal_price: pd.DataFrame, param: int, call_method: str):
+        super().__init__(signal, rebal_price, param)
+
+        # 최적화 기법을 호출하면 목적함수를 자동으로 부를 수 있게 딕셔너리로 관리한다.
+        # self.odjective_dict = {
+        #                     'msr': [self.msr, self.optimizing_weights(self.neg_sharpe)], 
+        #                     'gmv': [self.gmv, self.optimizing_weights(self.port_vol)], 
+        #                     'mdp': [self.mdp, self.optimizing_weights(self.neg_div_ratio)], 
+        #                     'rp': [self.rp, self.optimizing_weights(self.msd_risk)]
+        #                     }
+
+        # # scipy.optimize의 minimize 사용하기 위한 변수들
+        # # 1. 목적함수: 목적함수에 따라 최적화 기법의 이름이 정해진다고 보면 된다.
+        # # 목적함수에 공통으로 필요한 속성 정의
+        # self.temp_weight = np.repeat(1/self.noa, self.noa)
+        # self.target_risk = np.repeat(1/self.noa, self.noa)
+
+        # # 2. 최적화의 초기갑
+        # self.init_guess = np.repeat(1/self.noa, self.noa)
+
+        # # 3. method : 사용할 알고리즘(솔버)를 나타내는 문자열
+        # self.method = 'SLSQP'
+
+        # # 4. bounds, constraints : 구속최적화 문제에서의 구속조건을 부과하
+        # self.weights_sum_to_1 = {'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}
+        # self.bounds = ((0.0, 1.0)) * self.noa
+        
+        ###### test
+        self.call_method = call_method
+        self.rebal_price = rebal_price
+        
+        add_total_signal = self.signal.copy()
+        add_total_signal['total_signal'] = add_total_signal.sum(axis=1)
+        self.add_total_signal = add_total_signal
+        
+    # opt_processing
+    def opt_processing(self, target_assets, present_date):
+        
+        method = self.call_method
+        past = pd.Timestamp(present_date) - DateOffset(years = 1)
+        present = pd.Timestamp(present_date) 
+        
+        rets = self.rebal_price.loc[past : present, target_assets].pct_change()
+        er = np.array(rets.mean() * self.param)
+        vol = np.array(rets.std() * np.sqrt(self.param))
+        cov = rets.cov() * self.param
+        
+        init_guess = np.repeat(1/len(target_assets), len(target_assets))
+        bounds = ((0,1) for i in range(len(target_assets)))
+        weights_sum_to_1 = ({'type': 'eq', 'fun': lambda x:  np.sum(x) - 1}) 
+
+        if method == 'msr':
+            def neg_sharpe(weights, er, cov):
+                r = weights.T @ er
+                vol = np.sqrt(weights.T @ cov @ weights)
+                return - r / vol
+
+            weights = minimize(neg_sharpe,
+                            init_guess,
+                            args=(er, cov),
+                            method='SLSQP',
+                            constraints=(weights_sum_to_1,), 
+                            bounds=bounds)
+
+            return weights.x
+        
+        elif method == 'gmv':
+            def port_vol(weights, cov):
+                return np.sqrt(weights.T @ cov @ weights)
+            
+            weights = minimize(port_vol, 
+                                init_guess,
+                                args=(cov), 
+                                method='SLSQP', 
+                                constraints=(weights_sum_to_1,), 
+                                bounds=bounds
+                                )
+
+            return weights.x
+        
+        elif method == 'mdp':
+            def neg_div_ratio(weights, vol, cov):
+                weighted_vol = weights.T @ vol
+                port_vol = np.sqrt(weights.T @ cov @ weights)
+                return - weighted_vol / port_vol
+            
+            weights = minimize(neg_div_ratio, 
+                                init_guess, 
+                                args=(vol, cov),
+                                method='SLSQP',
+                                constraints=(weights_sum_to_1,), 
+                                bounds=bounds)
+            
+            return weights.x
+
+        elif method == 'rp':
+            target_risk = np.repeat(1/len(target_assets), len(target_assets))
+            
+            def msd_risk(weights, target_risk, cov):
+                port_var = weights.T @ cov @ weights
+                marginal_contribs = cov @ weights
+                risk_contribs = np.multiply(marginal_contribs, weights.T) / port_var
+                w_contribs = risk_contribs
+                return ((w_contribs - target_risk)**2).sum()
+            
+            weights = minimize(msd_risk, 
+                                init_guess,
+                                args=(target_risk, cov), 
+                                method='SLSQP',
+                                constraints=(weights_sum_to_1,),
+                                bounds=bounds)
+            return weights.x
+        
+    def target_assets(self, series): 
+        signal = series.loc["total_signal"]
+        present_date = series["index"]
+        tickers = self.add_total_signal.columns.drop('total_signal')
+        #print(tickers)
+    
+        if signal:
+            target_assets = series.loc[tickers].sort_values().iloc[-int(signal):].index.tolist()
+            result = self.opt_processing(target_assets, present_date)
+            return pd.Series(result, index = target_assets)
+
+    def run(self):
+        final_weight = self.add_total_signal.reset_index().apply(self.target_assets, axis=1).fillna(0)
+        final_weight.index = self.add_total_signal.index
+        final_weight = final_weight.iloc[self.param:,]
+        return final_weight
+    
+# target_weight.index = test_rel_signal.index
+    # # optimizing_weights: minimize의 결과를 weight으로 받는다.
+    # def optimizing_weights(self, objective_func) -> pd.DataFrame:
+    #     """_summary_
+
+    #     Args:
+    #         objective_func (function): odjective_dict의 value의 2번째 값에 저장된 함수
+
+    #     Returns:
+    #         pd.DataFrame: 최적화가 완료 된 투자비중 df
+    #     """
+        
+    #     optimized_weights = minimize(
+    #                                 objective_func,
+    #                                 self.init_guess,
+    #                                 args=(self.er, self.cov),
+    #                                 method=self.method,
+    #                                 constraints=(self.weights_sum_to_1),
+    #                                 bounds=self.bounds
+    #                                 )
+    #     return optimized_weights.x
+    
+    # # 목적함수 종류: 마이너스 샤프비율, 포트폴리오 변동성, 마이너스 분산비율, 목표 위험 기여도와의 오차 최소화
+    # # 1.1) MSR(샤프비율 최대화)의 목적함수: 마이너스 샤프비율 
+    # def neg_sharpe(self):
+    #     # portfolio returns
+    #     port_returns = self.temp_weight.T @ self.er
+
+    #     # portfolio volatility
+    #     port_vol = np.sqrt(self.temp_weight.T @ self.cov @ self.temp_weight)
+
+    #     # portfolio sharpe ratio
+    #     port_sharpe = port_returns / port_vol
+
+    #     return port_sharpe
+
+    # # 1.2) GMV(최소 변동성)의 목적함수: 포트폴리오 변동성
+    # def port_vol(self):
+    #     # portfolio volatility
+    #     port_vol = np.sqrt(self.temp_weight.T @ self.cov @ self.temp_weight)
+
+    #     return port_vol
+
+    # # 1.3) MDP(최대 분산비율)의 목적함수: 마이너스 분산비율
+    # def neg_div_ratio(self):
+    #     # weighted_vol
+    #     weighted_vol = self.temp_weight.T @ self.vol
+
+    #     # portfolio volatility
+    #     port_vol = np.sqrt(self.temp_weight.T @ self.cov @ self.temp_weight)
+
+    #     return - weighted_vol / port_vol
+
+    # # 1.4) RP(리스크 패리티)의 목적함수: 목표 위험 기여도와의 오차 최소화
+    # def msd_risk(self):
+
+    #     port_var = self.temp_weight.T @ self.cov @ self.temp_weight
+    #     marginal_contribs = self.cov @ self.temp_weight
+
+    #     risk_contribs = np.multiply(
+    #         marginal_contribs, self.temp_weight.T) / port_var
+
+    #     w_contribs = risk_contribs
+    #     return ((w_contribs - self.target_risk)**2).sum()
+
+    # # MSR(샤프비율 최대화)
+    # def msr(self, optimizing_weights) -> pd.DataFrame:
+        
+    #     optimized_weights = optimizing_weights
+
+    #     return optimized_weights
+
+    # # GMV(최소 변동성)
+    # def gmv(self):
+
+    #     weights = self.optimized_weights
+
+    #     return weights.x
+
+    # # MDP(최대 분산비율)
+    # def mdp(self):
+
+    #     weights = self.optimized_weights
+
+    #     return weights.x
+
+    # # RP(리스크 패리티)의 목적함수: 목표 위험 기여도와의 오차 최소화
+    # def rp(self):
+
+    #     weights = self.optimized_weights
+
+    #     return weights.x
+    
+
+    # def run(self, series): 
+    #     signal = series.loc["_signal"]
+    #     present_date = series["index"]
+    #     tickers = self.rebal_price.columns.drop('_signal')
+
+    #     if signal:
+    #         target_assets = series.loc[tickers].sort_values().iloc[-int(signal):].index.tolist()
+    #         weights = self.run(target_assets, present_date)
+    #         return pd.Series(weights, index = target_assets)
+
+
+# from pandas.tseries.offsets import *
+
+# def res_cal(target_assets, present_date) : 
+#     past = pd.Timestamp(present_date) - DateOffset(years = 1)
+#     present = pd.Timestamp(present_date)
+#     print(past)
+#     print(present)
+#     ret_daily = test_rebal_price_df.loc[past : present, target_assets].pct_change()
+#     cov_daily = ret_daily.cov()
+        
+#     n_assets = len(target_assets)
+#     covmat=cov_daily*250
+#     weights =np.ones(n_assets)/n_assets
+#     bnds = ((0,1) for i in range(n_assets))
+#     cons = ({'type': 'eq', 'fun': lambda x:  np.sum(x) - 1}) 
+
+#     def obj_variance(weights, covmat):
+#         return np.sqrt(weights.T @ covmat @ weights)
+#     res = minimize(obj_variance, weights,(covmat), method='SLSQP', bounds=bnds, constraints=cons)
+#     return res.x
+
+
--- quant/strategy/optimize/TimeSeries.py
+++ quant/strategy/optimize/TimeSeries.py
@@ -1,50 +1,80 @@
+# 패키지 임포트
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from scipy.stats import norm
-from scipy.optimize import minimize
+import yfinance as yf
 
-# 종적 배분 모델 클래스
+from scipy.optimize import minimize
+from scipy.stats import norm
+
+
 class TimeSeries:
-    def __init__(self, port_rets, param):
+    # 종적 배분 클래스
+
+    def __init__(self, 
+                port_rets,
+                param: int,
+                cs_port_rets: pd.Series,
+                cs_weights: pd.DataFrame,
+                rebal_price: pd.DataFrame,
+                ):
+
         self.port_rets = port_rets
         self.param = param
+        self.cs_port_rets = cs_port_rets
+        self.cs_weights = cs_weights
+        self.rets = rebal_price.pct_change().dropna()
 
-    # VT   
+        # 거래비용
+        self.cost = 0.0005
+
+    # VT
     def vt(self, vol_target=0.1):
         vol = self.port_rets.rolling(self.param).std().fillna(0) * np.sqrt(self.param)
-        weights = (vol_target / vol).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
+        weights = (
+            vol_target / vol).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
         weights[weights > 1] = 1
         return weights
-    
+
     # CVT
-    def cvt(self, delta=0.01, cvar_target=0.05):
+    def cvt(self, port_rets, param, delta=0.01, cvar_target=0.05):
         def calculate_CVaR(rets, delta=0.01):
-            VaR = rets.quantile(delta)    
+            VaR = rets.quantile(delta)
             return rets[rets <= VaR].mean()
-        
-        rolling_CVaR = -self.port_rets.rolling(self.param).apply(calculate_CVaR, args=(delta,)).fillna(0)
-        weights = (cvar_target / rolling_CVaR).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
+
+        rolling_CVaR = - \
+            port_rets.rolling(param).apply(
+                calculate_CVaR, args=(delta,)).fillna(0)
+        weights = (
+            cvar_target / rolling_CVaR).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
         weights[weights > 1] = 1
         return weights
-    
+
     # KL
-    def kl(self):
-        sharpe_ratio = (self.port_rets.rolling(self.param).mean() * np.sqrt(self.param) / self.port_rets.rolling(self.param).std())
-        weights = pd.Series(2 * norm.cdf(sharpe_ratio) - 1, index=port_rets.index).fillna(0)
+    def kl(self, port_rets, param):
+        sharpe_ratio = (port_rets.rolling(param).mean() *
+                        np.sqrt(param) / port_rets.rolling(param).std())
+        weights = pd.Series(2 * norm.cdf(sharpe_ratio) - 1,
+                            index=port_rets.index).fillna(0)
         weights[weights < 0] = 0
         weights = weights.shift(1).fillna(0)
         return weights
-    
+
     # CPPI
-    def cppi(self, m=3, floor=0.7, init_val=1):
-        n_steps = len(self.port_rets)
+
+    def cppi(self, port_rets, m=3, floor=0.7, init_val=1):
+        n_steps = len(port_rets)
         port_value = init_val
         floor_value = init_val * floor
         peak = init_val
-
-        port_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
-        weight_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
-        floor_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
+        """
+        init_val = 스타트 시점
+        m = 래버리지 승수
+        floor = 하한선 비율 (e.g. init_val 이 1이고 floor이 0.7이면 하한선은 30%)
+        """
+        port_history = pd.Series(dtype=np.float64).reindex_like(port_rets)
+        weight_history = pd.Series(dtype=np.float64).reindex_like(port_rets)
+        floor_history = pd.Series(dtype=np.float64).reindex_like(port_rets)
 
         for step in range(n_steps):
             peak = np.maximum(peak, port_value)
@@ -55,10 +85,56 @@ class TimeSeries:
 
             risky_alloc = port_value * weight
             safe_alloc = port_value * (1 - weight)
-            port_value = risky_alloc * (1 + self.port_rets.iloc[step]) + safe_alloc
+            port_value = risky_alloc * (1 + port_rets.iloc[step]) + safe_alloc
 
             port_history.iloc[step] = port_value
             weight_history.iloc[step] = weight
             floor_history.iloc[step] = floor_value
 
         return weight_history.shift(1).fillna(0)
+
+    def transaction_cost(self, weights_df, rets_df, cost=0.0005):
+        # 이전 기의 투자 가중치
+        prev_weights_df = (weights_df.shift(1).fillna(0) * (1 + rets_df.iloc[self.param-1:, :])) \
+            .div((weights_df.shift(1).fillna(0) * (1 + rets_df.iloc[self.param-1:, :])).sum(axis=1), axis=0)
+
+        # 거래비용 데이터프레임
+        cost_df = abs(weights_df - prev_weights_df) * cost
+        cost_df.fillna(0, inplace=True)
+        return cost_df
+
+    # 백테스팅 실행 함수
+
+    def run(self, ts_model, cost):
+        # 빈 딕셔너리
+        backtest_dict = {}
+
+        # 일별 수익률 데이터프레임
+        rets = self.rets
+
+        # 종적 배분 모델 선택 및 실행
+        if ts_model == 'VT':
+            ts_weights = self.vt(self.cs_port_rets, self.param)
+        elif ts_model == 'CVT':
+            ts_weights = (self.cvt(self.cs_port_rets, self.param))
+        elif ts_model == 'KL':
+            ts_weights = (self.kl(self.cs_port_rets, self.param))
+        elif ts_model == 'CPPI':
+            ts_weights = (self.cppi(self.cs_port_rets))
+        elif ts_model == None:
+            ts_weights = 1
+
+        # 최종 포트폴리오 투자 가중치
+        port_weights = self.cs_weights.multiply(ts_weights, axis=0)
+
+        # 거래비용 데이터프레임
+        cost = self.transaction_cost(port_weights, rets)
+
+        # 최종 포트폴리오 자산별 수익률
+        port_asset_rets = port_weights.shift() * rets - cost
+
+        # # 최종 포트폴리오 수익률
+        port_rets = port_asset_rets.sum(axis=1)
+        port_rets.index = pd.to_datetime(port_rets.index).strftime("%Y-%m-%d")
+
+        return port_weights, port_asset_rets, port_rets
