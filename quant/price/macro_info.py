f5033973c23ea5a897bf91f7fb2b679ed2f87bca
cswcjt <jungtaek0227@gmail.com>
2023-02-08 21:18:39 +0900

    feat: Add Macro Indicator

--- quant/price/macro_info.py
+++ quant/price/macro_info.py
@@ -1,8 +1,17 @@
 import pandas as pd
 from fredapi import Fred
 
-def get_econ_info(indicators) : 
-    fred = Fred(api_key=)
+def get_econ_info(indicators: dict={'cli': 'USALOLITONOSTSAM', 
+                                    'core_pce': 'PCEPILFE', 
+                                    'sahm': 'SAHMREALTIME', 
+                                    'fed_rate':'FEDFUNDS'
+                                    }
+                ): 
+    
+    with open('../fredapikey.txt', 'r') as f:
+        fred_key = f.read()
+    
+    fred = Fred(api_key=fred_key)
     temp = []
     for indicator in indicators : 
         target_df = pd.DataFrame({f'{indicator}': fred.get_series(f'{indicator}')})
@@ -12,3 +21,5 @@ def get_econ_info(indicators) :
     econ_df.columns = indicators
 
     return econ_df
+
+
