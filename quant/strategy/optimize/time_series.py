99452662942c9db29908ec27614143fcfe3c7a39
cswcjt <jungtaek0227@gmail.com>
2023-02-14 06:22:28 +0900

    feat: Add Weight Method

--- quant/strategy/optimize/time_series.py
+++ quant/strategy/optimize/time_series.py
@@ -1,8 +1,15 @@
 # 패키지 임포트
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from scipy.stats import norm
+import sys
+sys.path.append('/Users/jtchoi/Library/CloudStorage/GoogleDrive-jungtaek0227@gmail.com/My Drive/quant/Quant-Project/quant')
+
+from price.price_processing import calculate_portvals, port_rets
+from strategy.optimize.cross_sectional import Equalizer
+
 
 class TimeSeries:
     """
@@ -10,134 +17,133 @@ class TimeSeries:
     횡적 자산배분으로 투자자산 간 투자비중을 산출은 위험자산에 투자할 비중을 구하는 것이다.
     이후 최종적으로 위험자산과 안전자산(현금)의 비중으로 구하는 것이 종적 자산배분이다. 
     """
-    def __init__(self, port_rets: pd.Series, cs_weight: pd.DataFrame, param: int, call_method: int):
+    def __init__(self, port_rets: pd.Series, 
+                cs_weight: pd.DataFrame, 
+                risk_tolerance: str,
+                call_method: int,
+                ):
         """_summary_
-
         Args:
-            port_rets (pd.Series): 리밸런싱 날의 포트폴리오 수익률 정보를 담고 있는 pd series 
+            port_rets (pd.Series): 일별 포트폴리오 수익률 정보를 담고 있는 pd series 
             cs_weight (pd.DataFrame): 횡적 자산분배 이후 투자비중 df
-            param (int): 연율화를 위한 상수값 설정
+            risk_tolerance (str): 투자자의 위험선호도 -> 'aggressive', 'moderate', 'conservative' 중 하나의 값
             call_method (str): 종적 자산분배 모델 이름
         
         """
         self.port_rets = port_rets
         self.cs_weight = cs_weight
-        self.param = param
         self.call_method = call_method
         
-    def ew(self, weight_target: int=0.7) -> pd.DataFrame:
+        weight_dict = {'aggressive': {'ew': 0.7},
+                        'moderate': {'ew': 0.5},
+                        'conservative': {'ew': 0.3}
+                        }
+        self.target_risk = weight_dict[risk_tolerance][call_method]
+        
+    def ew(self) -> pd.DataFrame:
         """_summary_
-
         Args:
-            weight_target (int, optional): 위험자산에 투자할 비중. Defaults to 0.7.
-
+            risk_tolerance (dict): 
+                - 'aggressive', 'moderate', 'conservative' 중 하나의 값
         Returns:
             pd.DataFrame: 투자비중 df
         """
-        
-        weights = pd.DataFrame({'PORTFOLIO': weight_target}, index=self.port_rets.index)
+        weights = pd.DataFrame({'PORTFOLIO': self.target_risk}, index=self.port_rets.index)
         
         return weights
 
-    def vt(self, vol_target: int=0.1) -> pd.DataFrame:
-        """_summary_
+    # def vt(self, vol_target: int=0.1) -> pd.DataFrame:
+    #     """_summary_
+    #     Args:
+    #         vol_target (int, optional): 
+    #             vt를 위한 변수. 포트폴리오의 타켓 변동성 설정값. Defaults to 0.1.
+    #     Returns:
+    #         pd.DataFrame: 투자비중 df
+    #     """
 
-        Args:
-            vol_target (int, optional): 
-                vt를 위한 변수. 포트폴리오의 타켓 변동성 설정값. Defaults to 0.1.
+    #     vol = self.port_rets.rolling(self.param).std().fillna(0) * np.sqrt(self.param)
+    #     weights = (vol_target / vol).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
+    #     weights[weights > 1] = 1
+    #     return weights
 
-        Returns:
-            pd.DataFrame: 투자비중 df
-        """
-
-        vol = self.port_rets.rolling(self.param).std().fillna(0) * np.sqrt(self.param)
-        weights = (vol_target / vol).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
-        weights[weights > 1] = 1
-        return weights
-
-    def cvt(self, delta: int=0.01, cvar_target: int=0.05) -> pd.DataFrame:
-        """_summary_
-
-        Args:
-            delta (int, optional): 
-                cvt를 위한 변수. 변동성의 quantile이라고 보면 된다. Defaults to 0.01.
-            cvar_target (int, optional): 
-                cvt를 위한 변수. 포트폴리오의 타겟 cvar 설정값. Defaults to 0.05.
-
-        Returns:
-            pd.DataFrame: 투자비중 df
-        """
+    # def cvt(self, delta: int=0.01, cvar_target: int=0.05) -> pd.DataFrame:
+    #     """_summary_
+    #     Args:
+    #         delta (int, optional): 
+    #             cvt를 위한 변수. 변동성의 quantile이라고 보면 된다. Defaults to 0.01.
+    #         cvar_target (int, optional): 
+    #             cvt를 위한 변수. 포트폴리오의 타겟 cvar 설정값. Defaults to 0.05.
+    #     Returns:
+    #         pd.DataFrame: 투자비중 df
+    #     """
         
-        def calculate_CVaR(rets: int, delta: int):
-            """_summary_
-
-            Args:
-                rets (int):  pd.Series인 self.port_rets를 row별로 슬라이싱한 int 값
-                delta (int): apply 함수 사용할 때 들어가는 변수로 rets.quantile(delta) 계산시 사용 
-
-            Returns:
-                _type_: 
-            """
+    #     def calculate_CVaR(rets: int, delta: int):
+    #         """_summary_
+    #         Args:
+    #             rets (int):  pd.Series인 self.port_rets를 row별로 슬라이싱한 int 값
+    #             delta (int): apply 함수 사용할 때 들어가는 변수로 rets.quantile(delta) 계산시 사용 
+    #         Returns:
+    #             _type_: 
+    #         """
             
-            VaR = rets.quantile(delta)
-            return rets[rets <= VaR].mean()
+    #         VaR = rets.quantile(delta)
+    #         return rets[rets <= VaR].mean()
 
-        rolling_CVaR = -self.port_rets.rolling(self.param).apply(calculate_CVaR, args=(delta, )).fillna(0)
-        weights = (cvar_target / rolling_CVaR).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
+    #     rolling_CVaR = -self.port_rets.rolling(self.param).apply(calculate_CVaR, args=(delta, )).fillna(0)
+    #     weights = (cvar_target / rolling_CVaR).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
         
-        # 레버리지 사용 불가
-        weights[weights > 1] = 1
+    #     # 레버리지 사용 불가
+    #     weights[weights > 1] = 1
         
-        return weights
+    #     return weights
 
-    # KL
-    def kl(self):
-        sharpe_ratio = (self.port_rets.rolling(self.param).mean() *
-                        np.sqrt(self.param) / self.port_rets.rolling(self.param, ).std())
-        weights = pd.Series(2 * norm.cdf(sharpe_ratio) - 1,
-                            index=self.port_rets.index).fillna(0)
-        weights[weights < 0] = 0
-        weights = weights.shift(1).fillna(0)
-        return weights
+    # # KL
+    # def kl(self):
+    #     sharpe_ratio = (self.port_rets.rolling(self.param).mean() *
+    #                     np.sqrt(self.param) / self.port_rets.rolling(self.param, ).std())
+    #     weights = pd.Series(2 * norm.cdf(sharpe_ratio) - 1,
+    #                         index=self.port_rets.index).fillna(0)
+    #     weights[weights < 0] = 0
+    #     weights = weights.shift(1).fillna(0)
+    #     return weights
+    
+    # # CPPI
+        # CPPI
+    # def cppi(self, m: int=3, floor: int=0.7, init_val=1) -> pd.DataFrame:
+    #     """_summary_
+    #     Args:
+    #         m (int, optional): 래버리지 승수. Defaults to 3.
+    #         floor (int, optional): 하한선 비율(e.g. init_val 이 1이고 floor이 0.7이면 하한선은 30%). Defaults to 0.7.
+    #         init_val (int, optional): 포트폴리오의 시작 가치를 1로 설정. Defaults to 1.
+    #     Returns:
+    #         pd.DataFrame: 투자비중 df
+    #     """
 
-    # CPPI
-    def cppi(self, m: int=3, floor: int=0.7, init_val=1) -> pd.DataFrame:
-        """_summary_
+    #     n_steps = len(self.port_rets)
+    #     port_value = init_val
+    #     floor_value = init_val * floor
+    #     peak = init_val
 
-        Args:
-            m (int, optional): 래버리지 승수. Defaults to 3.
-            floor (int, optional): 하한선 비율(e.g. init_val 이 1이고 floor이 0.7이면 하한선은 30%). Defaults to 0.7.
-            init_val (int, optional): 포트폴리오의 시작 가치를 1로 설정. Defaults to 1.
+    #     port_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
+    #     weight_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
+    #     floor_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
 
-        Returns:
-            pd.DataFrame: 투자비중 df
-        """
+    #     for step in range(n_steps):
+    #         peak = np.maximum(peak, port_value)
+    #         floor_value = peak * floor
 
-        n_steps = len(self.port_rets)
-        port_value = init_val
-        floor_value = init_val * floor
-        peak = init_val
+    #         cushion = (port_value - floor_value) / port_value
+    #         weight = m * cushion
 
-        port_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
-        weight_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
-        floor_history = pd.Series(dtype=np.float64).reindex_like(self.port_rets)
+    #         risky_alloc = port_value * weight
+    #         safe_alloc = port_value * (1 - weight)
+    #         port_value = risky_alloc * (1 + self.port_rets.iloc[step]) + safe_alloc
 
-        for step in range(n_steps):
-            peak = np.maximum(peak, port_value)
-            floor_value = peak * floor
+    #         port_history.iloc[step] = port_value
+    #         weight_history.iloc[step] = weight
+    #         floor_history.iloc[step] = floor_value
 
-            cushion = (port_value - floor_value) / port_value
-            weight = m * cushion
-
-            risky_alloc = port_value * weight
-            safe_alloc = port_value * (1 - weight)
-            port_value = risky_alloc * (1 + self.port_rets.iloc[step]) + safe_alloc
-
-            port_history.iloc[step] = port_value
-            weight_history.iloc[step] = weight
-            floor_history.iloc[step] = floor_value
-
-        return weight_history.shift(1).fillna(0)
+    #     return weight_history.shift(1).fillna(0)
     
     def run(self):
         method = self.call_method
@@ -146,17 +152,17 @@ class TimeSeries:
         if method == 'ew':
             ts_weight = self.ew()
             
-        elif method == 'vt':
-            ts_weight = self.vt()
+        # elif method == 'vt':
+        #     ts_weight = self.vt()
             
-        elif method == 'cvt': 
-            ts_weight = self.cvt()
+        # elif method == 'cvt': 
+        #     ts_weight = self.cvt()
             
-        elif method == 'kl':
-            ts_weight = self.kl()
+        # elif method == 'kl':
+        #     ts_weight = self.kl()
             
-        elif method == 'cppi': 
-            ts_weight = self.cppi()
+        # elif method == 'cppi': 
+        #     ts_weight = self.cppi()
             
         # ts weight만 확인
         ts_weight = pd.concat([ts_weight, 1-ts_weight], axis=1, join='inner')
@@ -168,3 +174,35 @@ class TimeSeries:
         cs_ts_port_weight.dropna(inplace=True)
         
         return ts_weight, cs_ts_port_weight
+    
+    
+if __name__ == '__main__':
+    path = '/Users/jtchoi/Library/CloudStorage/GoogleDrive-jungtaek0227@gmail.com/My Drive/quant/Quant-Project/quant'
+    equity_df = pd.read_csv(path + '/equity_universe.csv', index_col=0)
+    equity_df.index = pd.to_datetime(equity_df.index)
+    equity_universe = equity_df.loc['2011':,].dropna(axis=1)
+    signal = pd.read_csv(path + '/result/mom_signal.csv', index_col=0)
+    
+    ew_weight = Equalizer(signal, equity_universe, 'quarter').ew()
+    
+    factor_portval = calculate_portvals(price_df=equity_universe, 
+                                        weight_df=ew_weight, 
+                                        signal_df=signal,
+                                        long_only=True
+                                        )
+    factor_cum_rets = port_rets(factor_portval, cumulative=True)       
+
+    ts_weights, ts_ew_weight = TimeSeries(port_rets=factor_cum_rets, 
+                                        cs_weight=ew_weight, 
+                                        risk_tolerance='conservative',
+                                        call_method='cppi', 
+                                        )\
+                                        .run()       
+    print(ts_weights)
+    print()
+    print(ts_ew_weight[(ts_ew_weight.sum(axis=1) != 1)])
+    print()
+    print(ts_ew_weight[(ts_ew_weight.sum(axis=1) != 1)].index)
+    print(ts_ew_weight[(ts_ew_weight.sum(axis=1) != 1)].sum(axis=1))
+    print()
+    print(ew_weight.loc[ts_ew_weight[(ts_ew_weight.sum(axis=1) != 1)].index].sum(axis=1))
