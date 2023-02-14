a1d283ca59346b3e7dbd7518d2a3765caa00902f
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-14 22:10:30 +0900

    refector: api view function change to class
    1. market_api를 MarketAPIView 변경 및 get_date(테스트용), get, post method 추가
    2. portfolio_api를 PortfolioAPIView 변경 및 get_date(테스트용), get, post method 추가
    3. factor_api를 FactorAPIView 변경 및 get_date(테스트용), get, post method 추가

--- django/dashboard/apis.py
+++ django/dashboard/apis.py
@@ -1,93 +1,99 @@
 import random
 
-from django.http import JsonResponse
-from django.urls import reverse
-
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-def factor_api(request):
-    pass
+class FactorAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
 
 
-def market_api(request):
-    ''' 
-    APEXCHARTS 형식의 맞는 임의 값 생성해서 반환중 
-    대충 테스트를 위해서 만든거라 코드 개떡같음 
-    백테스팅해서 넘길 json 값을 APEXCHARTS 형식에 맞게 수정이 필요할 수도 있음
-    '''
+class MarketAPIView(APIView):
+    def get_data(self):
+        return {
+            'regime_clustering': {
+                'data': [{'name': name, 'data': [[random.randint(1, 11), random.randint(1, 11)] for j in range(1, 21)]} for name in ['setosa', 'versicolor', 'virginica']],
+                'type': 'scatter',
+                'height': 350,
+                'colors': ['#a9a0fc', '#FF6384', '#008FFB']
+            },
 
-    data = {
-        'regime_clustering': {
-            'data': [{'name': name, 'data': [[random.randint(1, 11), random.randint(1, 11)] for j in range(1, 21)]} for name in ['setosa', 'versicolor', 'virginica']],
-            'type': 'scatter',
-            'height': 350,
-            'colors': ['#a9a0fc', '#FF6384', '#008FFB']
-        },
+            'index_chart': {
+                'data': [{'name': name, 'data': random.sample(list(range(0, 101)), 11)} for name in ['AAPL', 'AMZN', 'GOOG', 'IMB', 'MSFT']],
+                'type': 'area',
+                'height': 190,
+                'colors': ['#a9a0fc', '#FF6384', '#008FFB', '#fdfd96', '#bfff00']
+            },
 
-        'index_chart': {
-            'data': [{'name': name, 'data': random.sample(list(range(0, 101)), 11)} for name in ['AAPL', 'AMZN', 'GOOG', 'IMB', 'MSFT']],
-            'type': 'area',
-            'height': 190,
-            'colors': ['#a9a0fc', '#FF6384', '#008FFB', '#fdfd96', '#bfff00']
-        },
+            'index_forecasting': {
+                'data': [{'name': name, 'data': random.sample(list(range(0, 51)), 10)} for name in ['AAPL', 'AMZN', 'GOOG', 'IMB', 'MSFT']],
+                'type': 'area',
+                'height': 190,
+                'colors': ['#a9a0fc', '#FF6384', '#008FFB', '#fdfd96', '#bfff00']
+            },
 
-        'index_forecasting': {
-            'data': [{'name': name, 'data': random.sample(list(range(0, 51)), 10)} for name in ['AAPL', 'AMZN', 'GOOG', 'IMB', 'MSFT']],
-            'type': 'area',
-            'height': 190,
-            'colors': ['#a9a0fc', '#FF6384', '#008FFB', '#fdfd96', '#bfff00']
-        },
+            'market_regime': {
+                'data': [{'name': f'Metric{i}', 'data': [{'x': f'w{j}', 'y': random.randint(0, 101)} for j in range(1, 11)]} for i in range(1, 6)],
+                'type': 'heatmap',
+                'height': 190,
+                'colors': ['#008FFB']
+            },
 
-        'market_regime': {
-            'data': [{'name': f'Metric{i}', 'data': [{'x': f'w{j}', 'y': random.randint(0, 101)} for j in range(1, 11)]} for i in range(1, 6)],
-            'type': 'heatmap',
-            'height': 190,
-            'colors': ['#008FFB']
-        },
+            'economic_growth': {
+                'data': [{'name': f'Metric{i}', 'data': [{'x': f'w{j}', 'y': random.randint(0, 101)} for j in range(1, 11)]} for i in range(1, 6)],
+                'type': 'heatmap',
+                'height': 190,
+                'colors': ['#008FFB']
+            },
+        }
 
-        'economic_growth': {
-            'data': [{'name': f'Metric{i}', 'data': [{'x': f'w{j}', 'y': random.randint(0, 101)} for j in range(1, 11)]} for i in range(1, 6)],
-            'type': 'heatmap',
-            'height': 190,
-            'colors': ['#008FFB']
-        },
-    }
+    def get(self, request, *args, **kwargs):
+        data = self.get_data()
+        return Response(data, status=status.HTTP_200_OK)
 
-    return JsonResponse(data=data, status=status.HTTP_200_OK)
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        data = self.get_data()
+        return Response(data=data, status=status.HTTP_200_OK)
 
 
-def portfolio_api(request):
-    ''' 
-    APEXCHARTS 형식의 맞는 임의 값 생성해서 반환중 
-    대충 테스트를 위해서 만든거라 코드 개떡같음 
-    백테스팅해서 넘길 json 값을 APEXCHARTS 형식에 맞게 수정이 필요할 수도 있음
-    '''
-    
-    names = ['Portfolilo', 'S&P500']
+class PortfolioAPIView(APIView):
+    def get_data(self):
+        names = ['Portfolilo', 'S&P500']
+        return {
+            'cumulative': {
+                'data': [{'name': name, 'data': random.sample(list(range(0, 101)), 11)} for name in names],
+                'type': 'area',
+                'height': 350,
+                'colors': ['#FF6384', '#00B1E4']
+            },
 
-    [{'name': name, 'data': random.sample(list(range(0, 101)), 11)} for name in names]
-    data = {
-        'cumulative': {
-            'data': [{'name': name, 'data': random.sample(list(range(0, 101)), 11)} for name in names],
-            'type': 'area',
-            'height': 350,
-            'colors': ['#FF6384', '#00B1E4']
-        },
+            'mdd': {
+                'data': [{'name': name, 'data': random.sample(list(range(0, 51)), 10)} for name in names],
+                'type': 'area',
+                'height': 190,
+                'colors': ['#a9a0fc', '#e2e2e2']
+            },
 
-        'mdd': {
-            'data': [{'name': name, 'data': random.sample(list(range(0, 51)), 10)} for name in names],
-            'type': 'area',
-            'height': 190,
-            'colors': ['#a9a0fc', '#e2e2e2']
-        },
+            'rolling_sharp_ratio': {
+                'data': [{'name': name, 'data': random.sample(list(range(0, 51)), 10)} for name in names],
+                'type': 'area',
+                'height': 190,
+                'colors': ['#a9a0fc', '#e2e2e2']
+            },
+        }
 
-        'rolling_sharp_ratio': {
-            'data': [{'name': name, 'data': random.sample(list(range(0, 51)), 10)} for name in names],
-            'type': 'area',
-            'height': 190,
-            'colors': ['#a9a0fc', '#e2e2e2']
-        },
-    }
+    def get(self, request, *args, **kwargs):
+        data = self.get_data()
+        return Response(data=data, status=status.HTTP_200_OK)
 
-    return JsonResponse(data=data, status=status.HTTP_200_OK)
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        data = self.get_data()
+        return Response(data=data, status=status.HTTP_200_OK)
+        
