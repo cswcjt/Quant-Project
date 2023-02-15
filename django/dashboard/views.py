23ac68ac7f0a046c0adbcfae940f6b5273496e22
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-16 04:40:51 +0900

    refector: deduplication metric div tag
    1. metric.html 생성 (중복 html 코드 제거를 위해서)
    2. views에서 metric 정보를 dict로 생성해서 templates로 넘김
    3. portfoilo.html metric div tag를 include 해서 사용

--- django/dashboard/views.py
+++ django/dashboard/views.py
@@ -20,8 +20,35 @@ def market(request):
 
 
 def portfolio(request):
-	return render(request, 'dashboard/container/portfolio.html', {
+    metric_first = {
+        'returns': 'Total Returns',
+        'cagr': 'CAGR',
+    }
+
+    metric_second = {
+        'mdd': 'MDD',
+        'mdd_duration': 'Underwater Period',
+        'volatility': 'Anuallized Volatility',
+    } 
+
+    metric_third = {
+        'sharp': 'Sharpe Ratio',
+        'sortino': 'Sortino Ratio',
+        'calmar': 'Calmar Ratio',
+    }
+
+    metric_last = {
+        'cvar_ratio': 'CVaR Ratio',
+        'hit': 'Hit Ratio',
+        'gtp': 'Gain-to-Pain'
+    }
+    
+    return render(request, 'dashboard/container/portfolio.html', {
         'app_name': 'portfolio',
         'api_url': reverse('dashboard:portfolio_api'),
         'checkboxs_info': set_checkboxs_info(),
+        'metric_first': metric_first,
+        'metric_second': metric_second,
+        'metric_third': metric_third,
+        'metric_last': metric_last,
     })
